b043aac16340d530f74d6b9e0cc4a21c00c29d0a
pawaffle <pdegterev@gmail.com>
2025-04-13 07:34:57 +0300

    task 11 init

---
+++ src/main/java/w4_t11_historyTrick/IMemento.java
@@ -0,0 +1,6 @@
+package w4_t11_historyTrick;
+
+public interface IMemento {
+    // memento metadata methods
+
+}
---
+++ src/main/java/w4_t11_historyTrick/Main.java
@@ -0,0 +1,7 @@
+package w4_t11_historyTrick;
+
+public class Main {
+    public static void main(String[] args) {
+        Gui.launch(Gui.class, args);
+    }
+}
